abaf704a7e8b9fec805947890b8fa2042127e219
yito <fiv.e2b3s@gmail.com>
2019-02-26 02:36:15 +0900

    subscribeできるように実装

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="chat">
+  <div id="chat">
     <div class="section teal lighten-4">
       <h1>chat</h1>
     </div>
 
     <div class="divider"></div>
-    <div class="section blue-grey lighten-4">
+    <div class="section blue-grey lighten-4" style="height: 500px; overflow: auto;">
       <div class="container">
-        <ul>
-          <li v-for="message in messages">
+        <ul id="messages-container" >
+          <li v-for="message in this.$data.messages">
             <div class="row z-depth-2 white" style="margin-bottom: 10px">
               <div class="col s9">
                 <p style="font-size: x-small">{{ dateFormat(message.create_date) }}</p>
@@ -27,7 +27,7 @@
           <div class="input-field col s9 offset-s3">
             <textarea v-model="inputMessage" id="textarea1" class="materialize-textarea"></textarea>
             <label for="textarea1">メッセージを入力してください</label>
-            <a style="float: right" class="waves-effect waves-light btn" @click="addMessage()">送信</a>
+            <a style="float: right" class="waves-effect waves-light btn" @click="registMessage()">送信</a>
           </div>
         </div>
       </div>
@@ -42,51 +42,60 @@ import { onCreateMessage } from '../graphql/subscriptions.js'
 
 const moment = require('moment')
 
-const __stringParseToMoment = ((stringDate) => {
-  return moment(stringDate, "YYYY-MM-DDTHH:mm:ss.SSSZ")
-})
-
 export default {
   name: 'chat',
   data() {
     return {
-      messages: []
+      messages: [],
+      inputMessage: "",
     }
   },
 
   mounted() {
-    const observer = this.$apollo.subscribe({
-      query: onCreateMessage,
+    this.$apollo.queries.messages.fetchMore({
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        this.updateMessages(fetchMoreResult.listMessages)
+        return
+      },
     })
 
-    observer.subscribe({
-      next(data) {
-        console.log(data.data.onCreateMessage)
-      },
-      error(error) {
-        console.error(error)
-      },
-    })
+    let container = this.$el.querySelector("#messages-container")
+    container.scrollIntoView(false)
+
   },
 
-
   methods: {
+    stringParseToMoment(stringDate) {
+      return moment(stringDate, "YYYY-MM-DDTHH:mm:ss.SSSZ")
+    },
     dateFormat(createDate) {
-      var m = __stringParseToMoment(createDate)
+      var m = this.stringParseToMoment(createDate)
       return m.format('YYYY/MM/DD HH:mm:ss');
     },
-    async addMessage() {
+    async registMessage() {
       createMessage(this.inputMessage)
+    },
+    updateMessages(newMessages) {
+      this.messages = this.listMessagesSorted(newMessages.items)
+    },
+    listMessagesSorted(data) {
+      return data.sort((o1, o2) => {
+        return moment(this.stringParseToMoment(o1.create_date)).isAfter(this.stringParseToMoment(o2.create_date)) ? 1 : -1
+      })
     },
   },
 
   apollo: {
     messages: {
-      query: () => listMessages,
-      update: data => {
-        return data.listMessages.items.sort((o1, o2) => {
-          return moment(__stringParseToMoment(o1.create_date)).isAfter(__stringParseToMoment(o2.create_date)) ? 1 : -1
-        })
+      query: listMessages,
+      update: function(data) {
+        return this.listMessagesSorted(data.listMessages.items)
+      },
+      subscribeToMore: {
+        document: onCreateMessage,
+        updateQuery: function(previousResult, { subscriptionData }) {
+          this.$data.messages.push(subscriptionData.data.onCreateMessage)
+        }
       }
     }
   },
