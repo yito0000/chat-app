2d3b593a05bc2a37af1646236a600444d30fffdd
yito <fiv.e2b3s@gmail.com>
2019-03-11 18:43:24 +0900

    不要なコード削除

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="app">
-    <!--<div id="nav">-->
-      <!--<router-link to="/">Home</router-link> |-->
-      <!--<router-link to="/about">About</router-link> |-->
-      <!--<router-link to="/login">Login</router-link>-->
-    <!--</div>-->
     <router-view/>
   </div>
 </template>
